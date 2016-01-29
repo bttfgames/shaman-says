429559c232f56f9a0e3adbdae34a5894bad3de94
Pedro Bueno <magodoido@gmail.com>
2016-01-28 23:47:05 -0200

    Adicionando GameManager e cenas de teste.

---
+++ Assets/_Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance = null;              //Static instance of GameManager which allows it to be accessed by any other script.
+    public GameObject Player;
+
+
+    //Awake is always called before any Start functions
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+        //Sets this to not be destroyed when reloading scene
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
---
+++ Assets/_Scripts/Loader.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Loader : MonoBehaviour {
+
+    public GameObject gameManager;          //GameManager prefab to instantiate.
+
+    void Awake()
+    {
+        if (GameManager.instance == null)
+        {
+            Instantiate(gameManager);
+        }
+    }
+
+}
