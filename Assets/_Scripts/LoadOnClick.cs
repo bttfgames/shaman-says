2eab74b7a97709b69e2594db7e8d3df8611e6d6d
Fabricio Brito <fabricio.brito@gmail.com>
2016-01-30 04:46:17 -0200

    Esqueleto Menu e Créditos

---
+++ Assets/_Scripts/LoadOnClick.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadOnClick : MonoBehaviour {
+
+	public GameObject loadingImage;
+
+	public void LoadScene(int level)
+	{
+		if(loadingImage != null)
+			loadingImage.SetActive (true);
+		
+		Application.LoadLevel (level);
+	}
+
+}
