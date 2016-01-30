538e1ffadf4e6dc9e76f04fc00707b164ce21837
Carlos <csneto@gmail.com>
2016-01-30 01:58:06 -0200

    Cena Gui

---
+++ Assets/_Scripts/GUIMeatGrade.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIMeatGrade : MonoBehaviour {
+
+    private string[] MeatGrade = {"Extra-Rare", "Rare", "Medium Rare", "Medium Well", "Well Done", "Over Cooked" };
+    private int MeatGradeIndex = 0;
+    public GameObject PlayerText;
+    
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void IncreaseMeatGrade ()
+    {
+        MeatGradeIndex++;
+        PlayerText.GetComponent<Text>().Text = MeatGrade[MeatGradeIndex];
+    }
+
+}
